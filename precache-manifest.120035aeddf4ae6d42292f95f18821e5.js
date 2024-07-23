79091d2b71c25a7e2c49b024030694778925fe1d
Namra Joshi <jnamra.nj@gmail.com>
2024-07-23 13:58:37 -0700

    Updates

---
+++ precache-manifest.120035aeddf4ae6d42292f95f18821e5.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bb8b58ee917961f01c2277b7edee8e3a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6c16021727defe7b831d",
+    "url": "/static/css/3.c558cd61.chunk.css"
+  },
+  {
+    "revision": "52dd6e240f90c93c6168",
+    "url": "/static/css/4.3df3000b.chunk.css"
+  },
+  {
+    "revision": "adf077ea904553771d64",
+    "url": "/static/css/main.c735ae60.chunk.css"
+  },
+  {
+    "revision": "32a4ab7ab4add8e1d1c7",
+    "url": "/static/js/2.e92709eb.chunk.js"
+  },
+  {
+    "revision": "6c16021727defe7b831d",
+    "url": "/static/js/3.6e7751e0.chunk.js"
+  },
+  {
+    "revision": "52dd6e240f90c93c6168",
+    "url": "/static/js/4.630bdcd5.chunk.js"
+  },
+  {
+    "revision": "adf077ea904553771d64",
+    "url": "/static/js/main.55d0d315.chunk.js"
+  },
+  {
+    "revision": "52f99cfbb99e9d907005",
+    "url": "/static/js/runtime~main.53c823da.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "276c0d5bc863c2b38f2e014aba4480b1",
+    "url": "/static/media/Namra Joshi Updated Resume Oct 2023 (1).276c0d5b.pdf"
+  },
+  {
+    "revision": "59d0092abfbd3ed4dd8fd97a6f8d62be",
+    "url": "/static/media/SJSU-logo.59d0092a.png"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
